24cd5b48e6b98c4669c6acee9820a1edf56d15c7
Jonatan Borkowski <jonatanborkowski@wp.pl>
2018-12-14 07:55:29 +0100

    added second part

--- src/main/scala/com/jobo/Main.scala
+++ src/main/scala/com/jobo/Main.scala
@@ -1,7 +1,7 @@
 package com.jobo
 
 import cats._
-import cats.effect.{ ConcurrentEffect, ContextShift, ExitCode, IOApp, Sync, Timer }
+import cats.effect.{ConcurrentEffect, ContextShift, ExitCode, IOApp, Sync, Timer}
 import cats.effect.concurrent.Ref
 import cats.syntax.all._
 
@@ -16,22 +16,29 @@ import org.http4s.server.blaze.BlazeBuilder
 import java.util.UUID
 
 case class User(name: String, age: Int)
+
 case class UpdateAge(age: Int)
 
 trait UserAlgebra[F[_]] {
   def find(username: String): F[Option[User]]
+
   def save(user: User): F[Unit]
+
   def updateAge(username: String, age: Int): F[Unit]
 }
 
 sealed trait UserError extends Exception
+
 case class UserAlreadyExists(username: String) extends UserError
+
 case class UserNotFound(username: String) extends UserError
+
 case class InvalidUserAge(age: Int) extends UserError
 
 // User Algebra interpreter
 
 object UserInterpreter {
+
   def create[F[_]](implicit F: Sync[F]): F[UserAlgebra[F]] =
     Ref.of[F, Map[String, User]](Map.empty[String, User]).map { state =>
       def validateAge(age: Int): F[Unit] =
@@ -43,16 +50,16 @@ object UserInterpreter {
 
         override def save(user: User): F[Unit] =
           validateAge(user.age) *>
-          find(user.name).flatMap {
-            case Some(_) =>
-              F.raiseError(UserAlreadyExists(user.name))
-            case None =>
-              state.update(_.updated(user.name, user))
-          }
+            find(user.name).flatMap {
+              case Some(_) =>
+                F.raiseError(UserAlreadyExists(user.name))
+              case None =>
+                state.update(_.updated(user.name, user))
+            }
 
         override def updateAge(username: String, age: Int): F[Unit] =
           validateAge(age) *>
-          find(username) flatMap {
+            find(username) flatMap {
             case None =>
               F.raiseError(UserNotFound(username))
             case Some(user) =>
@@ -66,9 +73,9 @@ import doobie._
 import doobie.implicits._
 import doobie.postgres.implicits._
 
-
 object UserInterpreterDoobie {
-  def create[F[_] :  MonadError[?[_], UserError]](transactor: Transactor[F]): UserAlgebra[F] = new UserAlgebra[F] {
+
+  def create[F[_]: MonadError[?[_], UserError]](transactor: Transactor[F]): UserAlgebra[F] = new UserAlgebra[F] {
     private def validateAge(age: Int): F[Unit] =
       if (age <= 0) MonadError[F, UserError].raiseError(InvalidUserAge(age)) else MonadError[F, UserError].unit
 
@@ -79,7 +86,9 @@ object UserInterpreterDoobie {
       }
 
     override def save(user: User): F[Unit] =
-      fr"""INSERT INTO public.user ( name, age ) VALUES ( ${user.name}, ${user.age} )""".update.run.transact(transactor).map( _ => ())
+      fr"""INSERT INTO public.user ( name, age ) VALUES ( ${user.name}, ${user.age} )""".update.run
+        .transact(transactor)
+        .map(_ => ())
 
     override def updateAge(username: String, age: Int): F[Unit] =
       fr"""UPDATE user SET age=$age WHERE name=username""".update.run.transact(transactor).map(_ => ())
@@ -87,7 +96,8 @@ object UserInterpreterDoobie {
 
 }
 
-class UserRoutes[F[_] : Sync](userAlgebra: UserAlgebra[F]) extends Http4sDsl[F] {
+class UserRoutes[F[_]: Sync](userAlgebra: UserAlgebra[F]) extends Http4sDsl[F] {
+
   val routes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "users" / username =>
       userAlgebra.find(username).flatMap {
@@ -107,7 +117,8 @@ class UserRoutes[F[_] : Sync](userAlgebra: UserAlgebra[F]) extends Http4sDsl[F] 
   }
 }
 
-class UserRoutesAlt[F[_] : Sync](userAlgebra: UserAlgebra[F]) extends Http4sDsl[F] {
+class UserRoutesAlt[F[_]: Sync](userAlgebra: UserAlgebra[F]) extends Http4sDsl[F] {
+
   val routes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "users" / username =>
       userAlgebra.find(username).flatMap {
@@ -116,21 +127,26 @@ class UserRoutesAlt[F[_] : Sync](userAlgebra: UserAlgebra[F]) extends Http4sDsl[
       }
 
     case req @ POST -> Root / "users" =>
-      req.as[User].flatMap { user =>
-        userAlgebra.save(user) *> Created(s"User ${user.name} created".asJson)
-      }.handleErrorWith {
-        case UserAlreadyExists(username) => Conflict(s"User name $username already exists".asJson)
-      }
+      req
+        .as[User]
+        .flatMap { user =>
+          userAlgebra.save(user) *> Created(s"User ${user.name} created".asJson)
+        }
+        .handleErrorWith {
+          case UserAlreadyExists(username) => Conflict(s"User name $username already exists".asJson)
+        }
 
     case req @ PUT -> Root / "users" / username =>
-      req.as[UpdateAge].flatMap { d =>
-        userAlgebra.updateAge(username, d.age) *> Ok(username.asJson)
-      }.handleErrorWith {
-        case InvalidUserAge(age) => BadRequest(s"Provided age is invalid $age".asJson)
-      }
+      req
+        .as[UpdateAge]
+        .flatMap { d =>
+          userAlgebra.updateAge(username, d.age) *> Ok(username.asJson)
+        }
+        .handleErrorWith {
+          case InvalidUserAge(age) => BadRequest(s"Provided age is invalid $age".asJson)
+        }
   }
 }
-
 
 trait HttpErrorHandler[F[_], E <: Throwable] {
   def handle(routes: HttpRoutes[F]): HttpRoutes[F]
@@ -140,7 +156,8 @@ object HttpErrorHandler {
   def apply[F[_], E <: Throwable](implicit ME: MonadError[F, E]) = ME
 }
 
-class UserRoutesMEOW[F[_] : Sync : Monad](userAlgebra: UserAlgebra[F])(implicit ev: HttpErrorHandler[F, UserError]) extends Http4sDsl[F] {
+class UserRoutesMEOW[F[_]: Sync: Monad](userAlgebra: UserAlgebra[F])(implicit ev: HttpErrorHandler[F, UserError])
+    extends Http4sDsl[F] {
   private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "users" / username =>
       userAlgebra.find(username).flatMap {
@@ -162,24 +179,27 @@ class UserRoutesMEOW[F[_] : Sync : Monad](userAlgebra: UserAlgebra[F])(implicit 
   val routes: HttpRoutes[F] = ev.handle(httpRoutes)
 }
 
-
 import cats.ApplicativeError
 import cats.data.{Kleisli, OptionT}
 
 object RoutesErrorHandler {
-  def apply[F[_], E <: Throwable](routes: HttpRoutes[F])(handler: E => F[Response[F]])(implicit AE: ApplicativeError[F, E]): HttpRoutes[F] =
+
+  def apply[F[_], E <: Throwable](routes: HttpRoutes[F])(handler: E => F[Response[F]])(
+    implicit AE: ApplicativeError[F, E]): HttpRoutes[F] =
     Kleisli { req: Request[F] =>
       OptionT {
-        routes.run(req).value.handleErrorWith { e => handler(e).map(Option(_)) }
+        routes.run(req).value.handleErrorWith { e =>
+          handler(e).map(Option(_))
+        }
       }
     }
 }
 
-class UserHttpErrorHandler[F[_] : MonadError[?[_], UserError]] extends HttpErrorHandler[F, UserError] with Http4sDsl[F] {
+class UserHttpErrorHandler[F[_]: MonadError[?[_], UserError]] extends HttpErrorHandler[F, UserError] with Http4sDsl[F] {
   private val handler: UserError => F[Response[F]] = {
     case UserAlreadyExists(username) => Conflict(s"Provided User ($username) already exists".asJson)
-    case InvalidUserAge(age) => BadRequest(s"Invalid age $age".asJson)
-    case UserNotFound(username) => NotFound(s"User not found: $username".asJson)
+    case InvalidUserAge(age)         => BadRequest(s"Invalid age $age".asJson)
+    case UserNotFound(username)      => NotFound(s"User not found: $username".asJson)
   }
 
   override def handle(routes: HttpRoutes[F]): HttpRoutes[F] =
@@ -203,20 +223,26 @@ object ErrorChannel {
     }
 
   object syntax {
-    implicit class ErrorChannelOps[F[_] : ErrorChannel[?[_], E], E <: Throwable](e: E) {
+
+    implicit class ErrorChannelOps[F[_]: ErrorChannel[?[_], E], E <: Throwable](e: E) {
       def raise[A]: F[A] = ErrorChannel[F, E].raise[A](e)
     }
+
   }
+
 }
 
-abstract class UserAlg[F[_] : ErrorChannel[?[_], E], E <: Throwable] {
+abstract class UserAlg[F[_]: ErrorChannel[?[_], E], E <: Throwable] {
   def find(username: String): F[Option[User]]
+
   def save(user: User): F[Unit]
+
   def updateAge(username: String, age: Int): F[Unit]
 }
 
 object UserInterpreterErrorChannel {
-  def mkUserAlg[F[_] : Sync](implicit error: ErrorChannel[F, UserError]): F[UserAlg[F, UserError]] =
+
+  def mkUserAlg[F[_]: Sync](implicit error: ErrorChannel[F, UserError]): F[UserAlg[F, UserError]] =
     Ref.of[F, Map[String, User]](Map.empty).map { state =>
       new UserAlg[F, UserError] {
         private def validateAge(age: Int): F[Unit] =
@@ -248,18 +274,18 @@ object UserInterpreterErrorChannel {
 
 abstract class RoutesImproved[F[_], E <: Throwable](implicit H: HttpErrorHandler[F, E]) extends Http4sDsl[F] {
   protected def httpRoutes: HttpRoutes[F]
+
   val routes: HttpRoutes[F] = H.handle(httpRoutes)
 }
 
-abstract class UserRoutesImproved[F[_] : HttpErrorHandler[?[_], E], E <: Throwable] (
+abstract class UserRoutesImproved[F[_]: HttpErrorHandler[?[_], E], E <: Throwable](
   users: UserAlg[F, E]
 ) extends RoutesImproved[F, E]
 
-
-class UserRoutesAltImproved[F[_] : HttpErrorHandler[?[_], UserError] : Sync](
+class UserRoutesAltImproved[F[_]: HttpErrorHandler[?[_], UserError]: Sync](
   users: UserAlg[F, UserError]
 ) extends UserRoutesImproved(users) {
-  protected val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F]{
+  protected val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] {
     case GET -> Root / "users" / username =>
       users.find(username).flatMap {
         case Some(user) => Ok(user.asJson)
@@ -282,15 +308,17 @@ class UserRoutesAltImproved[F[_] : HttpErrorHandler[?[_], UserError] : Sync](
   }
 }
 
-
 sealed trait CatalogError extends Exception
+
 case class ItemAlreadyExists(item: String) extends CatalogError
+
 case class CatalogNotFound(id: Long) extends CatalogError
 
 case class Item(name: String) extends AnyVal
 
 abstract class CatalogAlg[F[_]: ErrorChannel[?[_], E], E <: Throwable] {
   def find(id: Long): F[List[Item]]
+
   def save(id: Long, item: Item): F[Unit]
 }
 
@@ -336,12 +364,14 @@ class CoUserRoutesMLTA[F[_]: Sync](
   private val httpRoutes: HttpRoutes[F] = ???
 
   import shapeless._
+
   def routes(implicit CH: CoHttpErrorHandler[F, UserError :+: CatalogError :+: CNil]): HttpRoutes[F] =
     CH.handle(httpRoutes)
 }
 
 abstract class CoRoutes[F[_], E <: Coproduct](implicit CH: CoHttpErrorHandler[F, E]) extends Http4sDsl[F] {
   protected def httpRoutes: HttpRoutes[F]
+
   val routes: HttpRoutes[F] = CH.handle(httpRoutes)
 }
 
@@ -357,7 +387,7 @@ abstract class CoUserRoutes[
 
 // type CustomerError = UserError :+: CatalogError :+: CNil
 
-class CoUserRoutesMLT[F[_] : CoHttpErrorHandler[?[_], UserError :+: CatalogError :+: CNil]](
+class CoUserRoutesMLT[F[_]: CoHttpErrorHandler[?[_], UserError :+: CatalogError :+: CNil]](
   users: UserAlg[F, UserError],
   catalogs: CatalogAlg[F, CatalogError]
 ) extends CoUserRoutes(users, catalogs) {
@@ -385,17 +415,15 @@ object Server extends IOApp {
     for {
       userInterp <- UserInterpreter.create[IO]
       doobieInterpreter = UserInterpreterDoobie.create[IO](transactor)
-      userRoutes = new UserRoutesMEOW[IO](doobieInterpreter)
+      userRoutes        = new UserRoutesMEOW[IO](doobieInterpreter)
       blaze <- BlazeBuilder[IO]
-             .bindHttp(8080, "0.0.0.0")
-             .mountService(userRoutes.routes, "/")
-             .serve
-             .compile
-             .drain
-             .as(ExitCode.Success)
+        .bindHttp(8080, "0.0.0.0")
+        .mountService(userRoutes.routes, "/")
+        .serve
+        .compile
+        .drain
+        .as(ExitCode.Success)
     } yield blaze
 
   }
 }
-
-
